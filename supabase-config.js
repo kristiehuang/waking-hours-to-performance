2877641632643fb9db92b571ddf541a3ecaabaac
Kristie Huang <kristiehuang01@gmail.com>
2025-08-27 11:33:19 -0400

    use public anon key 2

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@ export const SUPABASE_URL = 'https://hempirkrllmcwydslbqn.supabase.co';
 
 // This is the public anon key - safe to expose
 // Security is handled by Row Level Security (RLS) policies in Supabase
-export const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlbXBpcmtybGxtY3d5ZHNsYnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDM0MzMsImV4cCI6MjA3MTg3OTQzM30.w0X8DmwdI1o0EckxA6KRXoWXhe3Bmfah-ZNqWhn7j1M
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlbXBpcmtybGxtY3d5ZHNsYnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDM0MzMsImV4cCI6MjA3MTg3OTQzM30.w0X8DmwdI1o0EckxA6KRXoWXhe3Bmfah-ZNqWhn7j1M"
 
 // Table name
 export const TRADING_TABLE = 'trading_performance';
